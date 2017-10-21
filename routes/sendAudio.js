28b399d0918b5682f5c046d97fee3a6b5ed89f52
Sergei Ten <sergeidten@gmail.com>
2017-10-21 08:50:55 -0400

    Fixing receiving logic

--- routes/sendAudio.js
+++ routes/sendAudio.js
@@ -4,17 +4,18 @@ var router = express.Router();
 router.post('/', function(req, res, next) {
     const Speech = require('@google-cloud/speech');
     speech = Speech({
-        projectId: "tdld-183601"
+        projectId: "tdld-183601",
+        keyFilename: "tldl-33486c6ff6e3.json"
       });
     console.log(req);
     res.sendStatus(200);
     const config = {
-        encoding: "AMR_WB",
+        encoding: "LINEAR16",
         sampleRateHertz: 16000,
         languageCode: "en_US"
       };
       const audio = {
-        content: req.body
+        content: req.body.audioString
       };
     
       const request = {
@@ -25,20 +26,13 @@ router.post('/', function(req, res, next) {
       speech.recognize(request)
       .then((data) => {
           console.log(data);
-        /*const response = data[0];
+        const response = data[0];
         response.results.forEach((result) => {
           console.log(`Transcription: `, result.alternatives[0].transcript);
           result.alternatives[0].words.forEach((wordInfo) => {
-            // NOTE: If you have a time offset exceeding 2^32 seconds, use the
-            // wordInfo.{x}Time.seconds.high to calculate seconds.
-            const startSecs = `${wordInfo.startTime.seconds}` + `.` +
-                (wordInfo.startTime.nanos / 100000000);
-            const endSecs = `${wordInfo.endTime.seconds}` + `.` +
-                (wordInfo.endTime.nanos / 100000000);
             console.log(`Word: ${wordInfo.word}`);
-            console.log(`\t ${startSecs} secs - ${endSecs} secs`);
           });
-        });*/
+        });
       })
       .catch((err) => {
         console.error('ERROR:', err);
